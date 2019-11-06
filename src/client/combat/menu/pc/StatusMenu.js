f1d40392b2ef7288cd6b015d2cd9ef93c85dd793
SorryTechies <k-l-l@mail.ru>
2019-11-07 00:00:15 +0300

    [Client] - Additional info in MapPage.

--- src/client/combat/menu/pc/StatusMenu.js
+++ src/client/combat/menu/pc/StatusMenu.js
@@ -32,15 +32,53 @@ export default class StatusMenu extends React.Component {
         </table>
     }
 
+    renderHealth() {
+        /** @type CombatData */
+        const data = this.props.unit.data;
+        const maxHealth = data.health + data.aHealth;
+        const currentHealth = maxHealth - data.lDamage - data.nDamage;
+        const lDam = data.lDamage > 0 ? "- " + data.lDamage.toString() : "";
+        const nDam = data.nDamage > 0 ? (lDam ? lDam : "- 0") + "/" + data.nDamage.toString() + " damage" : "";
+        const aHp = (data.aHealth > 0 ? "+ " + data.aHealth.toString() + " bonus" : "");
+        return <div>{`Health: ${currentHealth}/${maxHealth}hp ${nDam} ${aHp}`}</div>
+    }
+
+    renderDefense() {
+        /** @type DefenseObject */
+        const defense = this.props.unit.character.defense;
+        if (!defense) return;
+        return <div>
+            <div>{`AC: ${defense.AC} TAC: ${defense.TAC} FFAC: ${defense.FFAC} TFFAC: ${defense.FFTAC}`}</div>
+            <div>{`CMD: ${defense.CMD}`}</div>
+        </div>
+    }
+
+    renderStats() {
+        const character = this.props.unit.character;
+        if (!character) return;
+        return <div>
+            {this.renderDefense()}
+        </div>
+    }
+
+    renderData() {
+        /** @type CombatObject */
+        const unit = this.props.unit;
+        if (!unit || !unit.data) return console.warn("No data found.");
+        if (!LoginController.isDM() && !unit.character) return;
+        return <div>
+            {this.renderHealth()}
+            {this.renderStats()}
+        </div>
+    }
+
     render() {
         const unit = this.props.unit;
         if (!unit) return null;
-        const character = unit.character;
-        const content = character ? `${unit.name} Damaged: ${character.damage}` : unit.name;
         const dmTools = LoginController.isDM() ? <button onClick={this.props.dmCallback}>+</button> : null;
         return <div className={`${rootScss.static_element} ${rootScss.combat_menu}`}>
             <div>{'Initiative: ' + unit.initiative}</div>
-            {content}
+            {this.renderData()}
             {this.getStatusTable(unit.buffs)}
             {dmTools}
         </div>
