e52905414e9ffaf726676b479232badc9e697965
SorryTechies <k-l-l@mail.ru>
2019-11-17 15:51:48 +0300

    [Server] - Refactoring.

--- src/server/express/post/saveMapChanges.js
+++ src/server/express/post/saveMapChanges.js
@@ -9,30 +9,27 @@ const Game = require('../../parse/classes/Game');
 const WebSocketUser = require("../../wss/WebSocketServer");
 const ErrorClass = require('../../logic/ErrorClass');
 
-require('../../logic/express').getServerExpress().post('/saveObject', (req, res) => {
-    const error = new ErrorClass(res);
-    const user = req.access;
-    const body = req.body;
-    (async () => {
+require('../../logic/express').getServerExpress().post('/saveObject', async (req, res) => {
+    try {
+        const body = req.body;
         /** @type CombatObject */
-        const object = await new Parse.Query(CombatObject)
-            .get(body.objectId, {useMasterKey: true});
+        const object = await new Parse.Query(CombatObject).get(body.objectId, {useMasterKey: true});
         if (!object) throw Error('Object not found');
         object.x = body.x;
         object.y = body.y;
         object.data = body.data;
         object.buffs = body.buffs;
         await object.save();
-        return res.json({});
-    })()
-        .then(() => WebSocketUser.sendToEverybody(WebSocketUser.RELOAD_MAP_MESSAGE))
-        .catch((e) => error.send(e));
+        res.json({});
+        WebSocketUser.sendToEverybody(WebSocketUser.RELOAD_MAP_MESSAGE);
+    } catch (e) {
+        return new ErrorClass(res).send(e);
+    }
 });
 
-require('../../logic/express').getServerExpress().get('/nextTurn', (req, res) => {
-    const error = new ErrorClass(res);
-    const user = req.access;
-    (async () => {
+require('../../logic/express').getServerExpress().get('/nextTurn', async (req, res) => {
+    try {
+        const user = req.access;
         if (!user.isDM) return res.json({});
         const game = await new Parse.Query(Game)
             .equalTo(Game.DM_FIELD, user)
@@ -44,8 +41,9 @@ require('../../logic/express').getServerExpress().get('/nextTurn', (req, res) =>
         objects.sort((item1,item2) => item1.initiative - item2.initiative);
         const nextObject = objects.find(item => item.initiative < game.currentInitiative);
         game.map.currentInitiative = nextObject.initiative;
-        return game.map.save();
-    })()
-        .then(() => res.json({}))
-        .catch((e) => error.send(e));
+        await game.map.save();
+        res.json({});
+    } catch (e) {
+        return new ErrorClass(res).send(e);
+    }
 });
