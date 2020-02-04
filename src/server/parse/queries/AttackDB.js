270d9fb33b6e81fd8ce2dfa9359cf60eba60e3a0
SorryTechies <k-l-l@mail.ru>
2020-02-04 22:16:25 +0300

    [Server] - Parse removed.

--- src/server/parse/queries/AttackDB.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by LastBerserk on 26.01.2019.
- */
-
-const Parse = require('../ParseInit').getParse();
-const Attack = require("../classes/Attack");
-
-module.exports.loadAttacks = (attacks) =>
-    new Parse.Query(Attack).containedIn("objectId", attacks).find({useMasterKey: true});
