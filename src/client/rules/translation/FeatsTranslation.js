2decc54d04e739cc4d3f9da7b3b3a799baf72128
Vatan34346 <65093340+Vatan34346@users.noreply.github.com>
2020-05-16 08:32:48 -0700

    Update FeatsTranslation.js

--- src/client/rules/translation/FeatsTranslation.js
+++ src/client/rules/translation/FeatsTranslation.js
@@ -19,5 +19,18 @@ export default {
             name: "Combat Expertise (Combat)",
             description: "You can choose to take a –1 penalty on melee attack rolls and combat maneuver checks to gain a +1 dodge bonus to your Armor Class. When your base attack bonus reaches +4, and every +4 thereafter, the penalty increases by –1 and the dodge bonus increases by +1. You can only choose to use this feat when you declare that you are making an attack or a full-attack action with a melee weapon. The effects of this feat last until your next turn."
         },
+    },
+    [SupportedLanguages.RU]: {
+        [RuleFeatsConstants.AXE_TO_GRIND]: {
+            name: "Axe to grind",
+            description: " Ты полуяаешь +1 трейт бонус к урону против врагов которые находятся в радиусе твоей атаки. "
+        },
+        [RuleFeatsConstants.CARETAKER]: {
+            name: "Caretaker",
+            description: "Ты получаешь +1  трейт бонус на лечение и лечение становится классвым навыком."
+        },
+        [RuleFeatsConstants.COMBAT_EXPERTISE]: {
+            name: "Combat Expertise (Combat)",
+            description: " Ты можешь выбрать получить -1 к атаке в ближнем бою и к боевым маневрам, чтобы получить +1 бонус уворота к броне.Когда бонус к атаке становится +4 и каждые +4 почле него, твой штраф увеличивется на -1, а уворот увеличивается на +1.  Вы можете использовать это умение только тогда, когда заявляете, что совершаете атаку или действие полной атаки с использованием оружия ближнего боя. Эффект этого умения длится до вашего следующего хода."
     }
 };
